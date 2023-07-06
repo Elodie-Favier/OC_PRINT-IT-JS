6756cb3ee741bbe86f3de3975b187afbe12ad6a1
Elodie Favier <elodie.favier@mailbox.org>
2023-07-06 15:56:14 +0200

    les bullets points suivent et marchent !

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,32 @@
 //**recup des images et tagline depuis fichier json */
 const slides = [
 	{	
-		"id": 1,
 		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"id": 2,
 		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"id": 3,
 		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"id": 4,
 		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let bulletsPoints = document.querySelectorAll("#banner .dots .dot")
+console.log(bulletsPoints)
+bulletsPoints[0].setAttribute("id", "first-dot")
+bulletsPoints[0].classList.add("dot_selected")
+bulletsPoints[1].setAttribute("id", "second-dot")
+bulletsPoints[2].setAttribute("id", "third-dot")
+bulletsPoints[3].setAttribute("id", "fourth-dot")
+
+let indexBulletsPoints = bulletsPoints.length
+console.log(indexBulletsPoints)
 
 //**Ecoute des évenements click sur les fleches */
 //**let listArrow = document.querySelectorAll(".arrow img")
@@ -31,36 +37,57 @@ const slides = [
 //**		console.log(`Ma flèche de ${myArrow.id}`)
 //**	})
 //**}
-let i = 0
-
+let indexSlidesShow = 0
+indexBulletsPoints = 0
 let imageBanner = document.querySelector("#banner .banner-img")
 let texteBanner = document.querySelector("#banner p")
+
+
 let arrowRight = document.querySelector(".arrow_right")
  arrowRight.addEventListener("click", function() {
-	i++
-	imageBanner.src = slides[i].image,
-	texteBanner.innerHTML = slides[i].tagLine,
+	indexSlidesShow++
+	imageBanner.src = slides[indexSlidesShow].image,
+	texteBanner.innerHTML = slides[indexSlidesShow].tagLine,
 	console.log("Changement d'image !")
+	indexBulletsPoints
+	bulletsPoints[indexBulletsPoints].classList.remove("dot_selected")
+	indexBulletsPoints++
+	bulletsPoints[indexBulletsPoints].classList.add("dot_selected")
+	
+	
 }
 
  )
  let arrowLeft = document.querySelector(".arrow_left")
 	arrowLeft.addEventListener("click", function() {
-		i--
-		imageBanner.src = slides[i].image,
-		texteBanner.innerHTML = slides[i].tagLine,
+		indexSlidesShow--
+		imageBanner.src = slides[indexSlidesShow].image,
+		texteBanner.innerHTML = slides[indexSlidesShow].tagLine,
 		console.log("vous êtes revenu à l'image d'avant!")
+		indexBulletsPoints
+		bulletsPoints[indexBulletsPoints].classList.remove("dot_selected")
+		indexBulletsPoints--
+		bulletsPoints[indexBulletsPoints].classList.add("dot_selected")
+		
 	})
-//*if(arrowRight = onclick){
-	//*let id = 0
-	//*const visuels = slides[id]
 
-	
-	//*let texteBanner = document.querySelector("#banner p")
-	//*texteBanner.innerText = slides[id].tagLine
-	//*index++ 
-	//*Récupération des éléments du tableau
-	
+	//*if(bulletsPoints[indexBulletsPoints] != slides[indexSlidesShow]) {
+		//*bulletsPoints[indexBulletsPoints].classList.remove("dot_selected")
+	//*}
 
-	//*Récupération des éléments du DOM	
+
+	//*if(bulletsPoints[indexBulletsPoints] = slides[indexSlidesShow]) {
+		//*indexBulletsPoints--
+	//*bulletsPoints[indexBulletsPoints].classList.add("dot_selected")
+		
+//*	}else {
+	//*	bulletsPoints[indexBulletsPoints].classList.remove("dot_selected")
+
+	//*}
+
+
+
+
+
+
 
