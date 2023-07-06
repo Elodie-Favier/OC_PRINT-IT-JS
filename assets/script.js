82cca3426bbf76e3f6fde1760bd960d86d2ba21c
Elodie Favier <elodie.favier@mailbox.org>
2023-07-06 09:45:59 +0200

    les flèches marchent !

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,66 @@
-let arrowLeft = document.querySelector(".arrow_left")
-let arrowRight = document.querySelector(".arrow_right")
-
-//* Récupération des balises html img, p, span dans #banner
-
-let imageBanner = document.querySelector("#banner .banner-img")
-
-
-let texteBanner = document.querySelector("#banner p")
-
-let spanBanner = document.querySelector("#banner span")
-
-//**tableau slideshow */
+//**recup des images et tagline depuis fichier json */
 const slides = [
-	{
-		"image":"images/slideshow/slide1.jpg",
+	{	
+		"id": 1,
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"images/slideshow/slide2.jpg",
+		"id": 2,
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"images/slideshow/slide3.jpg",
+		"id": 3,
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"images/slideshow/slide4.png",
+		"id": 4,
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-
-
-//**on acceède à l'indice i de la liste slides pour configurer la source de l'image et la tagline */
-	//**imageBanner.src = slides[i].image
-	//**texteBanner = slides[i].tagLine 
-
-
 //**Ecoute des évenements click sur les fleches */
-let listArrow = document.querySelectorAll(".arrow img")
-for(let i = 0; i < listArrow.length; i++) {
-	let arrowClicked = listArrow[i]
-	arrowClicked.addEventListener("click", (event)=> {
-		myArrow = event.target
-		console.log(`Ma flèche de ${myArrow.id}`)
-	})
+//**let listArrow = document.querySelectorAll(".arrow img")
+//**for(let i = 0; i < listArrow.length; i++) {
+//**	let arrowClicked = listArrow[i]
+//**	arrowClicked.addEventListener("click", (event)=> {
+	//**	myArrow = event.target
+//**		console.log(`Ma flèche de ${myArrow.id}`)
+//**	})
+//**}
+let i = 0
+
+let imageBanner = document.querySelector("#banner .banner-img")
+let texteBanner = document.querySelector("#banner p")
+let arrowRight = document.querySelector(".arrow_right")
+ arrowRight.addEventListener("click", function() {
+	i++
+	imageBanner.src = slides[i].image,
+	texteBanner.innerHTML = slides[i].tagLine,
+	console.log("Changement d'image !")
 }
- 
-	if(arrowRight.addEventListener("click", () => {
-		for(let index = 0; index < slides.length; index++) {
-			imageBanner.src = slides[index].image
-			texteBanner = slides[index].tagLine 
-		}
-	}))
-	console.log("nouvelle image")
+
+ )
+ let arrowLeft = document.querySelector(".arrow_left")
+	arrowLeft.addEventListener("click", function() {
+		i--
+		imageBanner.src = slides[i].image,
+		texteBanner.innerHTML = slides[i].tagLine,
+		console.log("vous êtes revenu à l'image d'avant!")
+	})
+//*if(arrowRight = onclick){
+	//*let id = 0
+	//*const visuels = slides[id]
+
+	
+	//*let texteBanner = document.querySelector("#banner p")
+	//*texteBanner.innerText = slides[id].tagLine
+	//*index++ 
+	//*Récupération des éléments du tableau
+	
+
+	//*Récupération des éléments du DOM	
+
